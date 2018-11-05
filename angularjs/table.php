b3be5bcd1c676fa85fe8eabb1b36a1c904b00bb2
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-05 08:46:09 -0500

    update

--- angularjs/table.php
+++ angularjs/table.php
@@ -1,73 +1,21 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.5/angular.min.js"></script>
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 
-    <style>
-    table, th , td {
-      border: 1px solid grey;
-      border-collapse: collapse;
-      padding: 5px;
-    }
-    table tr:nth-child(odd) {
-      background-color: #ffffff;
-    }
-    table tr:nth-child(even) {
-      background-color: #afafaf;
-    }
-    </style>
+$conn = new mysqli("localhost", "andrew", "southhills#", "andrew");
 
-  </head>
-  <body>
-    <div ng-app="myApp" ng-controller="myCtrl">
-      My name is {{ (firstName + " " + lastName) | uppercase }}
-      <br />
-      <ul>
-        <li ng-repeat="x in myData">
-          {{x.Name}} - {{x.City}}, {{x.Country}}
-        </li>
-      </ul>
+$result = $conn->query("SELECT first_name, last_name, city FROM angular_people");
+$output = "";
 
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>City</th>
-            <th>Country</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr ng-repeat="x in myData | orderBy: 'City'">
-            <td>{{ x.Name }}</td>
-            <td>{{ x.City }}</td>
-            <td>{{ x.Country }}</td>
-          </tr>
-        </tbody>
-      </table>
-<br /><br />
+while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($output != "") {$output .= ",";}
+    $output .= '{"firstName":"'  . $row["first_name"] . '",';
+    $output .= '"lastName":"'   . $row["last_name"]        . '",';
+    $output .= '"city":"'. $row["city"]     . '"}';
+}
 
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr ng-repeat="x in myPHPData | orderBy: 'city'">
-            <td>{{ x.firstName }}</td>
-            <td>{{ x.lastName }}</td>
-            <td>{{ x.city }}</td>
-          </tr>
-        </tbody>
-      </table>
+$output = '{"records":[' .$output.']}';
+$conn->close();
 
-    </div>
-  </body>
-  <script src="myApp.js"></script>
-  <script src="myCtrl.js"></script>
-  <script src="table.php"></script>
-</html>
+echo($output);
+?>
