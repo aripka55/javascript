b381fd9a838eed4cad7b63a0dcca90234067b929
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-05 08:16:56 -0500

    update

--- angularjs/myCtrl.js
+++ angularjs/myCtrl.js
@@ -1,8 +1,8 @@
 app.controller("myCtrl", function($scope, $http){
-    $scope.firstName = "Andrew";
-    $scope.lastName = "Ripka";
+    //$scope.firstName = "Andrew";
+    //$scope.lastName = "Ripka";
 
-    $http.get("json_sample.html").then(function (response) {
+    $http.get("table.php").then(function (response) {
         $scope.myData = response.data.records;
     });
 });
---
+++ angularjs/table.php
@@ -0,0 +1,21 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+//header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new msqli("localhost", "andrew", "southhills#", "andrew");
+
+$result = $conn->query("SELECT user_id, first_name, last_name, city FROM angular_people");
+$output = "";
+
+while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($output != "") {$output .= ",";}
+    $output .= '{"First Name":"'  . $row["first_name"] . '",';
+    $output .= '"Last Name":"'   . $row["last_name"]        . '",';
+    $output .= '"City":"'. $row["city"]     . '"}';
+}
+
+$output = '{"Angular People":[' .$output.']}';
+$conn->close();
+
+echo($output);
+?>
