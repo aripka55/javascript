408873693002c6b9e646a7ec963056d467f47127
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-05 08:42:39 -0500

    update

--- angularjs/myCtrl.js
+++ angularjs/myCtrl.js
@@ -1,8 +1,12 @@
-app.controller("myCtrl", function($scope, $http){
-    //$scope.firstName = "Andrew";
-    //$scope.lastName = "Ripka";
+app.controller("myCtrl", function($scope, $http) {
+    $scope.firstName = "Andrew";
+    $scope.lastName= "Ripka";
+
+    $http.get("json_sample.html").then(function (response) {
+      $scope.myData = response.data.records;
+    });
 
     $http.get("table.php").then(function (response) {
-        $scope.myData = response.data.records;
-    });
+      $scope.myPHPData = response.data.records;
+    })
 });
