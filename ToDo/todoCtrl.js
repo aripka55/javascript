7ada460c43aa4dd5598b080014c0d088c86cf39f
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-18 17:40:53 -0500

    update

--- ToDo/todoCtrl.js
+++ ToDo/todoCtrl.js
@@ -1,6 +1,31 @@
-app.controller("todoCtrlctrl", function($scope, $http) {
+app.controller('todoCtrl', function($scope, $http) {
+  getTask(); // Loading tasks 
+  function getTask(){  
 
-    $http.get("todolistDatabase.php").then(function (response) {
-      $scope.myPHPData = response.data.records;
-    })
+  $http.post("gettask.php").success(function(data){
+    $scope.listid = data;
+    });
+  };
+
+  $scope.addTask = function (task) {
+    $http.post("adding.php?listid="+listid).success(function(data){
+      getTask();
+      $scope.taskInput = "";
+      });
+    };
+
+  $scope.deleteTask = function (task) {
+    if(confirm("Are you sure to delete this line?")){
+      $http.post("deletetask.php?taskID="+task).success(function(data){
+        getTask();
+      });
+    }
+  };
+
+  $scope.toggleStatus = function(item, status, task) {
+    if(status=='2'){status='0';}else{status='2';}
+      $http.post("updatetask.php?taskID="+item+"&status="+status).success(function(data){
+        getTask();
+      });
+  };
 });
